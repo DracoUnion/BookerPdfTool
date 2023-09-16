9f27e63cb447c0822013d9242e2126c169fe8a66
wizardforcel <562826179@qq.com>
2023-09-16 19:12:38 +0800

    2023-09-16 19:12:35

--- BookerPdfTool/dedup.py
+++ BookerPdfTool/dedup.py
@@ -3,7 +3,13 @@ import fitz
 from io import BytesIO
 import re
 import tempfile
+import os
+import hashlib
 from .util import *
+
+def md5_int(text):
+     hash_ = hashlib.md5(text.encode('utf8')).hexdigest()
+     return int(hash_, 16)
 
 def norm_l2(arr, axis=-1):
     l2 = (arr ** 2).sum(axis, keepdims=True) ** 0.5
@@ -11,7 +17,7 @@ def norm_l2(arr, axis=-1):
 
 def read_pdf_text_n(pdf, n=500):
     res = ''
-    for l in read_pdf_lines(data):
+    for l in read_pdf_lines(pdf):
         if len(res) >= n: break
         res += re.sub(r'\s', '', l)
     return res[:n]
@@ -20,7 +26,7 @@ def read_pdf_lines(pdf):
     # 全部读进来转成内存流避免锁文件
     if isinstance(pdf, str):
         pdf = open(pdf, 'rb').read()
-    doc: fitz.Document = fitz.open(BytesIO(pdf))
+    doc: fitz.Document = fitz.open('pdf', BytesIO(pdf))
     for pg in doc:
         for block in pg.get_text_blocks():
             yield block[4].strip()
@@ -30,14 +36,15 @@ def ngram_hash_freq(text, ngram=3, nbucket=1000):
     vec = np.zeros([nbucket], dtype=np.int32)
     # 将文本切分为 ngram，然后对于每个子串计算哈希值，映射到 nbucket 个桶中
     hashs = [
-        hash(text[i:i+ngram]) % nbucket 
+        md5_int(text[i:i+ngram]) % nbucket 
         for i in range(0, len(text) - ngram + 1)
     ]
     # 将哈希计数保存到数组中
     for h in hashs:
         vec[h] += 1
     # 将哈希计数变成哈希频率
-    return vec / vec.sum()
+    hash_ =  vec / vec.sum()
+    return hash_
 
 def dedup(fvecs, his_vecs=None, thres=0.9):
     if his_vecs is None:
@@ -45,11 +52,12 @@ def dedup(fvecs, his_vecs=None, thres=0.9):
     for it in fvecs:
         fname, fvec = it['name'], it['vec'][None]
         if len(his_vecs) == 0:
+            print(f'文件名：{fname}，相似度：无')
             his_vecs = np.vstack([his_vecs, fvec])
             continue
         sims = norm_l2(fvec) @ norm_l2(his_vecs).T
         maxsim = np.max(sims)
-        print(f'文件名: {fname}, 相似度: {maxsim}')
+        print(f'文件名：{fname}，相似度：{maxsim}')
         # 如果最大相似度小于阈值，判定不重复
         if maxsim < thres:
             # 将当前文档添加到历史向量及结果中
@@ -102,12 +110,12 @@ def dedup_handle(args):
         np.save(db_fname, np.zeros([0, 1000]))
     his_vecs = np.load(db_fname)
     assert (
-        isinstance(his_vecs, np.array)  
+        isinstance(his_vecs, np.ndarray)  
         and his_vecs.ndim == 2 
         and his_vecs.shape[1] == 1000
     )
     # 去重
-    his_vecs = dedup(fvec, his_vecs, args.thres)
+    his_vecs = dedup(fvecs, his_vecs, args.thres)
     # 保存历史向量库
     np.save(db_fname, his_vecs)
     
